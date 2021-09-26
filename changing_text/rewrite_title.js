30741ebc67bb79f3e9b214a7dee652d19a7f86ef
johnedstone <johnedstone@gmail.com>
2021-09-25 20:57:09 -0400

    finishing up

--- changing_text/rewrite_title.js
+++ changing_text/rewrite_title.js
@@ -3,6 +3,12 @@ jQuery(function( $ ){
 
     $("div p").text("Changing from Hello World to Goodbye World");
 
+    $( "input[value='acb3d0b0-16ce-4fd2-b579-fa6cea308e78']", function(){
+      console.log('Found input');
+    });
+
+    $( "input[value='acb3d0b0-16ce-4fd2-b579-fa6cea308e78']" ).parent().find("h5:contains('Choose your Amount')").text("changing to Hocus Pocus");
+
     $.get("rewrite_title.js",function(data,status){
         //alert("Data: " + data + "\nStatus: " + status);
         $("div pre").text(data);
